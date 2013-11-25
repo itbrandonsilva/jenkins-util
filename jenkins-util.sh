fc095b14eadbff0bdeb60a0414113507dbf62e72
Brandon Silva <itbrandonsilva@gmail.com>
2013-11-24 21:58:35 -0500

    Initial commit.

---
+++ jenkins-util.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e;
+case "$1" in
+
+
+"clone")
+    echo "Cloning...";
+    rm -rf app;
+    git clone $2 $3;
+    echo "Cloning complete.";
+    ;;
+
+
+"install")
+    echo "Deploying...";
+    bash ./$2/jenkins/install.sh;
+    echo "Deployment succesful.";
+    ;;
+
+
+"test")
+    echo "Testing...";
+    bash ./$2/jenkins/test.sh;
+    echo "Tests succesful.";
+    ;;
+
+
+"start")
+    echo "Starting...";
+    bash ./$2/jenkins/start.sh;
+    echo "Running.";
+    ;;
+
+
+*)
+    echo ERROR \> jenkins-util.sh: Invalid arg: \"$1\";
+    exit 1;
+    ;;
+
+
+esac
+
+exit 0;
